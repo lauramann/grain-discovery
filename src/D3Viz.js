b5b57eb83f06b3c0314b90524e4309f3c474cb19
lauramann <lauramann@trentu.ca>
2019-05-28 15:42:24 -0400

    revised code

--- src/D3Viz.js
+++ src/D3Viz.js
@@ -10,15 +10,16 @@ class D3Viz extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      numIter: 1000,
       pi: 0,
       r: 0,
       run: true,
       sliderVal: 100,
-      input: '1000',
+      numIter: '1000',
       squareColor: '#7F5AD1',
       circleColor: '#ACD15A'
     };
+
+    // function declarations
     this.setUp = this.setUp.bind(this);
     this.runSimulation = this.runSimulation.bind(this);
     this.sleep = this.sleep.bind(this);
@@ -27,21 +28,22 @@ class D3Viz extends React.Component {
     this.inputChange = this.inputChange.bind(this);
   }
 
+  // create d3 node (circle and square)
   componentDidMount() {
     this.setUp();
   }
 
+  // handles when user changes speed slider
   handleChange = (event, value) => {
     this.setState({ sliderVal: value });
-    // console.log(value)
   };
 
+  // handles when user enters new numIter value
   inputChange = (event) => {
-    // console.log(event.target.value)
-    this.setState({ input: event.target.value })
+    this.setState({ numIter: event.target.value })
   }
 
-  // REVISE
+  // setUp function creates d3 node (circle and square)
   setUp() {
     const node = this.node;
     select(node).html('');
@@ -71,7 +73,8 @@ class D3Viz extends React.Component {
       .style('fill', 'none');
   }
 
-  // Sleep function taken from https://davidwalsh.name/javascript-sleep-function
+  // Sleep function adapted from https://davidwalsh.name/javascript-sleep-function
+  // Takes number from slider value (0-100) and converts to corresponding speed
   sleep(ms) {
     let speed = 0
     if (ms == 0) speed = 1000
@@ -81,39 +84,39 @@ class D3Viz extends React.Component {
     return new Promise(resolve => setTimeout(resolve, speed));
   }
 
+  // runSimulation function starts the simulation
   async runSimulation() {
-    console.log("run called")
-    console.log(this.state.input)
-    this.setState({ run: false })
-    console.log(this.state.input)
     this.setUp()
     this.setState({ run: true })
-    const node = this.node
-    console.log("Number of iterations: " + this.state.input)
 
+    const node = this.node
+    let randNums, randX, randY, totCount, innerCount = 0
+    let i = this.state.numIter
+
+    // generate random numbers for x & y
     function getRandXY() {
       let x = Math.random()
       let y = Math.random()
       return [x, y];
     }
 
-    let randNums, randX, randY = 0
-    let totCount = 0
-    let innerCount = 0
-    // let i = this.state.numIter
-    let i = this.state.input
-
+    // While num iterations not at 0...
     do {
       i--
+      // sleep depending on what user sets slider value
       await this.sleep(this.state.sliderVal)
+
       totCount += 1
       randNums = getRandXY()
       randX = randNums[0] * this.props.width
       randY = randNums[1] * this.props.width
 
+      // Calculate whether random point is inside circle
+      // If it is, increase innerCount
       let insideCirc = Math.pow(randX - (this.props.width / 2), 2) + Math.pow(randY - (this.props.width / 2), 2) < (this.state.r * this.state.r)
       if (insideCirc) innerCount += 1
 
+      // append dot onto d3 node
       if (this.state.run) {
         select(node).append('circle')
           .attr('cx', randX)
@@ -121,19 +124,22 @@ class D3Viz extends React.Component {
           .attr('r', 1.5)
           .style('fill', insideCirc ? this.state.circleColor : this.state.squareColor);
 
+        // calculate updated pi value
         this.setState({ pi: (4.0 * (innerCount / totCount)) })
       }
     } while (this.state.run && i)
-
   }
 
+  // resetSimulation function resets simulation
   resetSimulation() {
+    // stop running and set pi to 0
     this.setState({ run: false, pi: 0 })
+
+    //clear the d3 viz
     this.setUp();
   }
 
   render() {
-
     return (
       <div className="App">
         <div className="left-side">
@@ -171,25 +177,23 @@ class D3Viz extends React.Component {
                 onChange={this.handleChange}
               />
             </div>
-
           </div>
 
           <div className="buttons">
-          <Button id="start-button" onClick={this.runSimulation} variant="contained">
-            Start Simulation
-      </Button>
-          <Button id="reset-button" onClick={this.resetSimulation} variant="contained">
-            Reset
-      </Button>
+            <Button id="start-button" onClick={this.runSimulation} variant="contained">
+              Start Simulation
+            </Button>
+            <Button id="reset-button" onClick={this.resetSimulation} variant="contained">
+              Reset
+            </Button>
           </div>
-          
         </div>
+
         <div className="right-side">
           <svg ref={node => this.node = node}
             width={this.props.width} height={this.props.height}>
           </svg>
         </div>
-
 
       </div>
     );
