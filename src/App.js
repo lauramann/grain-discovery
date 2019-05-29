9a60e99369a32c5d059c96ab0a33bbd479534847
lauramann <lauramann@trentu.ca>
2019-05-28 22:12:56 -0400

    added button to scroll down to auc

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './App.css';
 import PiEstimation from './PiEstimation';
 import AreaUnderCurve from './AreaUnderCurve';
-import {Button, Typography} from '@material-ui/core';
+import Fab from '@material-ui/core/Fab';
+import ScrollableAnchor from 'react-scrollable-anchor'
 
 class App extends React.Component {
   constructor(props) {
@@ -14,14 +15,24 @@ class App extends React.Component {
   handleClick(event) {
     console.log(event.target.id)
     this.setState({pageToShow: event.target.id})
-
   }
 
   render() {
     return (
       <div className="App">
+        <div className="fab">
+        <Fab href="#auc" id="fab" color="primary" aria-label="Add">
+        
+        Next
+      </Fab>
+      </div>
+      
         <PiEstimation width={500} height={500} />
-        <AreaUnderCurve width={500} height={500} />
+
+        <ScrollableAnchor id={'auc'}>
+        <AreaUnderCurve id="auc" width={500} height={500} />
+        </ScrollableAnchor>
+        
       </div>
     );
   }
